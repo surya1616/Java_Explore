0910e6433540eae2c5e2bc86c4d5c7d8168f72f6
surya <surya@LAPTOP-9AR5S2O3>
2017-04-18 00:15:50 -0500

    initial ss

---
+++ Java_Test/src/com/org_name/mainpkg/Main_Class.java
@@ -0,0 +1,11 @@
+package com.org_name.mainpkg;
+
+public class Main_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+	}
+
+}
